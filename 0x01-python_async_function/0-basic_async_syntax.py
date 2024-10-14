1c28f7eb11facf0d0bdbdb50834bb121cfb14b9b
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-14 13:29:13 +0000

    basi async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-
+"""
+    Waits for a random delay between 0 and max_delay seconds
+    and returns the delay.
+"""
 import asyncio
 import random
 
 async def wait_random(max_delay: int = 10)-> float:
     """
-    Waits for a random delay between 0 and max_delay seconds
-    and returns the delay.
-
     Args:
         max_delay (int): Maximum value for the random
         delay in seconds. Defaults to 10.
