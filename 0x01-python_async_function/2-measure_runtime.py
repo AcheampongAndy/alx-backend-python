ab28706b3a7e97e9dbb6c59f4fd27d3c4343ce0d
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-14 15:09:57 +0000

    measure

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -18,9 +18,9 @@ async def measure_time(n: int, max_delay: int) -> float:
     Returns:
         float: The average execution time per coroutine.
     """
-    start_time = time.time()
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
+    end_time = time.perf_counter()
     total_time = end_time - start_time
 
     return total_time / n
